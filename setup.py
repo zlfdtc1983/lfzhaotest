3a21cb30b9d3b86c13993981e3a01037f29eefe2
zlfdtc1983 <zlfdtc1983@126.com>
2019-11-27 14:41:42 +0800

    改版本号
    改版本号

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages, setup, Command
 # 神奇的操作，一个函数完事
 setup(
   name = "lfzhaotest",
-  version = "0.1.0",
+  version = "0.1.1",
   keywords = ("pip", "pathtool","lfzhaotest"),
   description = "lfzhao test",
   long_description = "a test for pip",
